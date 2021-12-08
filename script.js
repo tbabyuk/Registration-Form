2d5011440e7e59e6ac2d1b8db1445083917e3ff0
Terry Babyuk <tbabyuk@gmail.com>
2021-12-07 22:49:06 -0500

    added functions for email and password validation

--- script.js
+++ script.js
@@ -8,21 +8,36 @@ const form = document.getElementById("form");
 
 username.focus();
 
-//VALIDATION FUNCTIONS
+//ERROR/SUCCESS FUNCTIONS
 
 function showError(input, message) {
-    input.style.borderColor = "#cc3d23";
-    let inputGroup = input.parentElement;
-    let small = inputGroup.querySelector("small");
+    let formGroup = input.parentElement;
+    let small = formGroup.querySelector("small");
+    formGroup.className = "form-group error";
     small.textContent = message;
 }
 
 function showSuccess(input) {
-    input.style.borderColor = "#23bb23";
-    let inputGroup = input.parentElement;
-    let small = inputGroup.querySelector("small");
+    let formGroup = input.parentElement;
+    let small = formGroup.querySelector("small");
+    formGroup.className = "form-group success";
     small.textContent = "";
 }
+
+
+//VALIDATOR FUNCTIONS
+
+//Check email is valid
+function checkEmail(email) {
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+//Check password has number
+function checkForNumber(password) {
+    return /\d/g.test(password);
+}
+
 
 
 form.addEventListener("submit", (e) => {
@@ -31,25 +46,35 @@ form.addEventListener("submit", (e) => {
 
     //enter validation conditions
 
-    if(username.value === "") {
+    if (username.value === "") {
         showError(username, "Username is required");
+    } else if (username.value.length <= 6) {
+        showError(username, "Username must be longer than 6 characters");
     } else {
         showSuccess(username);
     }
 
-    if(email.value === "") {
+    if (email.value === "") {
         showError(email, "Email is required");
+
+        //check if entered email is valid
+    } else if (!checkEmail(email.value.trim())) {
+        showError(email, "Email is not valid. Try again.")
     } else {
         showSuccess(email);
     }
-    if(password.value === "") {
+    if (password.value === "") {
         showError(password, "Password is required");
+
+        //check if entered password includes at least one number    
+    } else if (!checkForNumber(password.value)) {
+        showError(password, "Password must contain at least one number");
     } else {
         showSuccess(password);
     }
-    if(password2.value === "") {
+    if (password2.value === "") {
         showError(password2, "Please re-enter password");
-    } else if(password.value !== password2.value) {
+    } else if (password.value !== password2.value) {
         showError(password2, "Passwords must match");
     } else {
         showSuccess(password2);
